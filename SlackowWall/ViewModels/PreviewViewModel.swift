5632fef821439f2d084813f3e3df6026336ca0c7
andrew <cowslayer6789@gmail.com>
2023-01-19 00:41:44 -0500

    buttons ignore input when switching focus to app now

--- SlackowWall/ViewModels/PreviewViewModel.swift
+++ SlackowWall/ViewModels/PreviewViewModel.swift
@@ -7,19 +7,21 @@ import SwiftUI
 class PreviewViewModel: ObservableObject {
 
     @MainActor func clickInstance(screenRecorder: ScreenRecorder, idx: Int) {
-        screenRecorder.capturePreviews[idx]
-        let pid = ShortcutManager.shared.instanceIDs[idx]
+        if (NSApplication.shared.isActive) {
+            screenRecorder.capturePreviews[idx]
+            let pid = ShortcutManager.shared.instanceIDs[idx]
 
-        let script = "tell application \"System Events\" to set frontmost of the first process whose unix id is \(pid) to true"
+            let script = "tell application \"System Events\" to set frontmost of the first process whose unix id is \(pid) to true"
 
-        var error: NSDictionary?
-        if let scriptObject = NSAppleScript(source: script) {
-            scriptObject.executeAndReturnError(&error)
-            ShortcutManager.shared.sendKey(key: 0x35, pid: pid)
-        } else {
-            print("Failed to send apple script")
+            var error: NSDictionary?
+            if let scriptObject = NSAppleScript(source: script) {
+                scriptObject.executeAndReturnError(&error)
+                ShortcutManager.shared.sendKey(key: 0x35, pid: pid)
+            } else {
+                print("Failed to send apple script")
+            }
+
+            print("pressed: \(pid) #(\(idx))")
         }
-
-        print("pressed: \(pid) #(\(idx))")
     }
 }
