6bb0360deffd7191df53a6c46dbabea37892561c
Slackow <32826498+Slackow@users.noreply.github.com>
2024-05-22 21:26:14 -0400

    make monitor size not have decimals

--- SlackowWall/Views/Settings/Behavior/BehaviorSettings.swift
+++ SlackowWall/Views/Settings/Behavior/BehaviorSettings.swift
@@ -39,7 +39,7 @@ struct BehaviorSettings: View {
                     SettingsToggleView(title: "Use State Output", description: "Turn this on if you have the state output mod, it prevents an instance from reseting if it is still generating the world.", option: $profileManager.profile.checkStateOutput)
                 }
                 
-                SettingsLabel(title: "Window Dimensions", description: "The dimensions of the game windows in different cases. These values should not exceed the current monitor size: [\(screenSize)](0).")
+                SettingsLabel(title: "Window Dimensions", description: "The dimensions of the game windows in different cases. These values should not exceed the current monitor size: [\(screenSize)](0).".replacingOccurrences(of: ".0", with: ""))
                     .tint(.orange)
                     .allowsHitTesting(false)
                     .contentTransition(.numericText())
