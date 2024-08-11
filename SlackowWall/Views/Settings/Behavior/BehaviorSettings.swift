9c6a2895065d8aa00128c1527777971f79abeece
Kihron <starlicblackhole@gmail.com>
2024-08-11 16:30:33 -0400

    - Adjusted description of utility mode

--- SlackowWall/Views/Settings/Behavior/BehaviorSettings.swift
+++ SlackowWall/Views/Settings/Behavior/BehaviorSettings.swift
@@ -20,7 +20,7 @@ struct BehaviorSettings: View {
             
             SettingsCardView {
                 VStack {
-                    SettingsToggleView(title: "Utility Mode", description: "Allows Non-Numbered instances to be captured by SlackowWall", descInteractable: false, option: $profileManager.profile.utilityMode)
+                    SettingsToggleView(title: "Utility Mode", description: "Allows Non-Numbered instances to be captured by SlackowWall.", descInteractable: false, option: $profileManager.profile.utilityMode)
                         .tint(profileManager.profile.f1OnJoin ? .red : .gray)
                         .animation(.easeInOut, value: profileManager.profile.f1OnJoin)
                     
