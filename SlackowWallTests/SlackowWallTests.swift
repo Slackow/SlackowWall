fa296de7b5c8f357b5e23889649644ac7f0bf970
Kihron <starlicblackhole@gmail.com>
2025-06-05 15:53:00 -0400

    - Added Swift Testing framework

---
+++ SlackowWallTests/SlackowWallTests.swift
@@ -0,0 +1,16 @@
+//
+//  SlackowWallTests.swift
+//  SlackowWallTests
+//
+//  Created by Kihron on 6/5/25.
+//
+
+import Testing
+
+struct SlackowWallTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
