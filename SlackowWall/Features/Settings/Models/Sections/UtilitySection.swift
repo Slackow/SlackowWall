814a3681f149af4d696f9102d9285f17fdd8aea5
Danny Nguyen <danny@odecloud.com>
2026-02-28 15:42:43 -0800

    add better projections for eye

--- SlackowWall/Features/Settings/Models/Sections/UtilitySection.swift
+++ SlackowWall/Features/Settings/Models/Sections/UtilitySection.swift
@@ -21,7 +21,10 @@ extension Preferences {
         var eyeProjectorStretchedOverlay: Bool = false
         var eyeProjectorOverlayImage: URL? = nil
         var eyeProjectorOverlayWidth: Int {
-            eyeProjectorStretchedOverlay ? 30 : 60
+            if eyeProjectorDynamicOverlay {
+                return eyeProjectorColumnsPerSide * 2
+            }
+            return eyeProjectorStretchedOverlay ? 30 : 60
         }
         var eyeProjectorAlwaysOnTop: Bool = false
         var eyeProjectorTitleBarHidden: Bool = false
@@ -29,9 +32,23 @@ extension Preferences {
             eyeProjectorEnabled && eyeProjectorOpenWithTallMode
         }
 
+        // Dynamic overlay (replaces static image when enabled)
+        var eyeProjectorDynamicOverlay: Bool = false
+        var eyeProjectorColumnsPerSide: Int = 15
+        var eyeProjectorOverlayColor1: CodableColor = CodableColor(r: 1.0, g: 0.69, b: 0.77)  // pink
+        var eyeProjectorOverlayColor2: CodableColor = CodableColor(r: 0.68, g: 0.85, b: 0.9)  // blue
+        var eyeProjectorOverlayTextColor: CodableColor = CodableColor(r: 0, g: 0, b: 0)  // black
+        var eyeProjectorOverlayCenterColor: CodableColor = CodableColor(r: 0.8, g: 0.8, b: 0.8)  // gray
+        var eyeProjectorOverlayBandOpacity: Double = 1.0
+        var eyeProjectorShowDecadeMarkers: Bool = false
+
         var pieProjectorEnabled: Bool = false
         var pieProjectorOpenWithTallMode: Bool = true
         var pieProjectorOpenWithThinMode: Bool = true
+        var pieProjectorFlatten: Bool = false
+        var pieProjectorScaleX: Double = 0.7
+        var pieProjectorScaleY: Double = 1.4
+        var pieProjectorOffsetY: Double = 40
         var pieProjectorECountScale: Double = 1.0
         var pieProjectorECountTranslation: CGSize = .zero
         var pieProjectorECountVisible: Bool = true
